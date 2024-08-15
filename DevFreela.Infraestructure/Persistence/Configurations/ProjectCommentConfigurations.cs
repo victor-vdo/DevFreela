de49c17086c32a7360f6a27bec69e83cd9aafa8f
Victor Oliveira <victor.vdo@outlook.com>
2024-08-15 16:49:50 -0300

    Add Entities Add DbContext Add DbSetConfigurations

---
+++ DevFreela.Infraestructure/Persistence/Configurations/ProjectCommentConfigurations.cs
@@ -0,0 +1,25 @@
+﻿using DevFreela.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DevFreela.Infraestructure.Persistence.Configurations
+{
+    public class ProjectCommentConfigurations : IEntityTypeConfiguration<ProjectComment>
+    {
+        public void Configure(EntityTypeBuilder<ProjectComment> builder)
+        {
+            builder
+                .HasKey(p => p.Id);
+
+            builder
+                .HasOne(p => p.Project)
+                .WithMany(p => p.Comments)
+                .HasForeignKey(u => u.IdProject);
+
+            builder
+                .HasOne(p => p.User)
+                .WithMany(u => u.Comments)
+                .HasForeignKey(u => u.IdUser);
+        }
+    }
+}
